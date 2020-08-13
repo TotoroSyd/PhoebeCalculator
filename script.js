6f017bdfc4543d054438dc78f54fe38b1a54ce22
PhoebeNguyen <phoebe.ngsyd@gmail.com>
2020-08-13 20:13:00 +1000

    Fixed bug when a second input is null. It caused NaN message

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ class Operation {
 
   numberButtonClicked(number) {
     // Assign number to num_input
+
+    // checking the operation status to identify if number input should go to second_input variable or not
     // the operator is not set. Update first_input
     if (this.operator === null) {
       // check if the first_input has value or not
@@ -23,7 +25,6 @@ class Operation {
       } else {
         this.first_input = this.first_input + number;
       }
-
       // display the number clicked
       this.displayScreen(this.first_input);
     }
@@ -41,21 +42,34 @@ class Operation {
   }
 
   decimalButtonClicked() {
+    // checking the operation status to identify if number input should go to second_input variable or not
     if (this.operator === null) {
+      // first_input has no value, set it as "0.x"
       if (this.first_input === null) {
         this.first_input = ".";
-      } else if (this.first_input.includes(".")) {
+      }
+      // first_input has value, it is a decimal number
+      else if (this.first_input.includes(".")) {
         return;
-      } else {
+      }
+      // first_input has value, it is not a decimal number
+      else {
         this.first_input = this.first_input + ".";
         this.displayScreen(this.first_input);
       }
-    } else {
+    }
+    // the operator has value => first_input is filled => assign value to second_input
+    else {
+      // second_input has no value, set it as "0.x"
       if (this.second_input === null) {
         this.second_input = ".";
-      } else if (this.second_input.includes(".")) {
+      }
+      // second_input has value, it is a decimal number
+      else if (this.second_input.includes(".")) {
         return;
-      } else {
+      }
+      // second_input has value, it is not a decimal number
+      else {
         this.second_input = this.second_input + ".";
         this.displayScreen(this.second_input);
       }
@@ -64,6 +78,7 @@ class Operation {
 
   equalButtonClicked() {
     this.calculate();
+    // reset operator to prevent program from breaking (doubble click =)
     this.operator = null;
   }
 
@@ -80,16 +95,21 @@ class Operation {
 
   calculate() {
     let action = this.operator;
-    // If the operator is null, do nothing
+    // If the operator is null, do nothing. Prevent double click equalButton that triggers calculate()
     if (this.operator === null) {
       return;
     }
-
     // When an operator is clicked but no number is chosen before that
     if (this.first_input === null) {
       return;
     }
+    // When second_input is null, do nothing even the method is called
+    if (this.second_input === null) {
+      return;
+    }
+
     // When a number is chosen before an operator is clicked
+    // convert string input into Float for the calculation
     const first = parseFloat(this.first_input);
     const second = parseFloat(this.second_input);
     let result;
@@ -102,11 +122,12 @@ class Operation {
     } else if (action === "divide") {
       result = this.divide(first, second);
     }
-
+    // cover scenario having more than multiple numbers and operators in the calculation
     this.first_input = result;
     this.second_input = null;
     this.displayScreen(result);
   }
+
   add(first, second) {
     return first + second;
   }
@@ -124,11 +145,13 @@ class Operation {
   }
 }
 
-// Execution
-document.addEventListener("DOMContentLoaded", function () {
+//================Execution=================
+// Ensure everything is ready
+document.addEventListener("DOMContentLoaded", () => {
+  // create new Operation class
+  const operation = new Operation();
   // queryselector the parent container of the buttons
   const calculator__keys = document.querySelector(".calculator__keys");
-  const operation = new Operation();
   // Attach eventlistener to the parent container to listen to all events of children
   calculator__keys.addEventListener("click", (event) => {
     // When there is a click event from any of the children, go find the child named "button"
@@ -137,19 +160,23 @@ document.addEventListener("DOMContentLoaded", function () {
       //Once found the child named "button", get the value of its attribute 'data-action'
       const button_clicked = event.target;
       const action = button_clicked.getAttribute("data-action");
+      // if a button is an operator button
       if (
         action === "add" ||
         action === "subtract" ||
         action === "multiply" ||
         action === "divide"
       ) {
+        // store the value of data-action into the global var
         operation.operator = action;
+        // cover scenario having more than multiple numbers and operators in the calculation
+        // once there are 2 inputs and an operator => run the calculate()
         if (operation.first_input !== null && operation.second_input !== null) {
           operation.calculate(action);
         } else {
           return;
         }
-        console.log(operation.operator);
+        // console.log(operation.operator);
       }
 
       if (action === "decimal") {
@@ -169,30 +196,3 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
-
-// Scenario
-// When a button !(.key--operator) clicked:
-//     if first!
-//     assign textContent of the button to first
-
-// if a button (.key--operator) clicked:
-//     convert attribute data-action to the operator (+,-,*,/)
-
-//     first + second = result
-// first - second = result
-// first * second = result
-// first / second = result
-
-// first / 0 ==> error
-
-// clear all
-
-// first operator_1 operator_2 second
-//     operator_2 replaces operator_1
-
-// first operator_1 second operator_2 third operator_3 fourth ....
-//     first operator_1 second = result_1
-//         first = result_1
-//         operator_1 = operator_2
-//         second = third
-//         repeat first operator second = result
